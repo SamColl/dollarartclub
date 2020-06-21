7894333bf5302ac99a86042ca6f561c176d65857
SamColl <sam_coll@hotmail.com>
2020-06-21 14:57:12 +0800

    Removed 'click me'

--- wp-content/themes/dollarartclub/grid.php
+++ wp-content/themes/dollarartclub/grid.php
@@ -20,7 +20,7 @@ if ( $the_query->have_posts() ) :
        $the_query->the_post();
        ?>
        <?php $postId = get_the_ID(); ?>
-       <div class="popup" onclick="myFunction()">Click me!
+       <div class="popup" onclick="myFunction()">
           <span class="popuptext" id="myPopup">Popup text...</span>
         </div>
        <?php
